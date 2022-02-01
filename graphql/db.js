561d581c9e0986d658ee3031d9b891eb12cb4d9f
jkwon <kwonejk@gmail.com>
2022-02-01 23:01:32 +0800

    Create Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -24,3 +24,23 @@ export const getById = (id) => {
     const filteredMovies = movies.filter(movie => movie.id === id); 
     return filteredMovies[0]; 
 };
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`, 
+        name,
+        score
+    }; 
+    movies.push(newMovie); 
+    return newMovie; 
+};
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id); 
+    if (movies.length > cleanedMovies.length) {
+        movies = cleanedMovies; 
+        return true; 
+    } else {
+        return false; 
+    }
+};
--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,10 +1,14 @@
-import { getById, getMovies } from './db';
+import { addMovie, deleteMovie, getById, getMovies } from './db';
 
 
 const resolvers = {
     Query: {
         movies: () => getMovies(), 
         movie: (_, { id }) => getById(id)
+    }, 
+    Mutation: {
+        addMovie: (_, { name, score }) => addMovie(name, score),
+        deleteMovie: (_, { id }) => deleteMovie(id)
     }
 }
 
